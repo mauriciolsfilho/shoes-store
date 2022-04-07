4b8d2159ef99807b713343aebd4e54273460d944
Maurício Linhares <mauricio_1souza@outlook.com>
2022-04-07 00:21:04 -0300

    feat: :sparkles: primeiras implementações para hook de carrinho

--- src/hooks/useCart.tsx
+++ src/hooks/useCart.tsx
@@ -1,7 +1,13 @@
-import { createContext, ReactNode, useContext, useState } from 'react';
-import { toast } from 'react-toastify';
-import { api } from '../services/api';
-import { Product, Stock } from '../types';
+import {
+  createContext,
+  ReactNode,
+  useContext,
+  useEffect,
+  useState,
+} from "react";
+import { toast } from "react-toastify";
+import { api } from "../services/api";
+import { Product, Stock } from "../types";
 
 interface CartProviderProps {
   children: ReactNode;
@@ -23,20 +29,49 @@ const CartContext = createContext<CartContextData>({} as CartContextData);
 
 export function CartProvider({ children }: CartProviderProps): JSX.Element {
   const [cart, setCart] = useState<Product[]>(() => {
-    // const storagedCart = Buscar dados do localStorage
+    const storagedCart = localStorage.getItem("@RocketShoes:cart");
 
-    // if (storagedCart) {
-    //   return JSON.parse(storagedCart);
-    // }
+    if (storagedCart) {
+      return JSON.parse(storagedCart);
+    }
 
     return [];
   });
 
+  const updateCartAndPersist = (value: Product[]): void => {
+    localStorage.setItem("@RocketShoes:cart", JSON.stringify(value));
+    setCart(value);
+  };
+
+  const getStockProduct = async (id: number): Promise<Stock> => {
+    const { data: stock } = await api.get<Stock>(`/stock/${id}`);
+    return stock;
+  };
+
+  const getProduct = async (id: number): Promise<Product> => {
+    const { data: product } = await api.get<Product>(`/products/${id}`);
+    return product;
+  };
+
   const addProduct = async (productId: number) => {
     try {
-      // TODO
+      const cartList = [...cart];
+      const stock = await getStockProduct(productId);
+
+      const prodExists = cartList.find((prod) => prod.id === productId);
+      const reqAmount = prodExists ? prodExists.amount + 1 : 1;
+
+      console.log(reqAmount, stock);
+      if (reqAmount > stock.amount) {
+        toast.error("Quantidade solicitada fora de estoque");
+        return;
+      }
+      const product = await getProduct(productId);
+
+      cartList.push({ ...product, amount: 1 });
+      updateCartAndPersist(cartList);
     } catch {
-      // TODO
+      toast.error("Erro na adição do produto");
     }
   };
 
@@ -44,7 +79,7 @@ export function CartProvider({ children }: CartProviderProps): JSX.Element {
     try {
       // TODO
     } catch {
-      // TODO
+      toast.error("Erro na remoção do produto");
     }
   };
 
@@ -55,7 +90,7 @@ export function CartProvider({ children }: CartProviderProps): JSX.Element {
     try {
       // TODO
     } catch {
-      // TODO
+      toast.error("Erro na alteração de quantidade do produto");
     }
   };
 
